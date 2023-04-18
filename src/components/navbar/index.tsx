9676d99da33288dd5ab983b282b16bf43a1307df
Guilherme Vargas <guisouzavargas@outlook.com>
2023-04-18 08:45:16 -0300

    end of navbar development

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
 
     return (
-        <nav className="flex justify-around mt-14">
+        <nav className="flex justify-around mt-14 items-center">
             <div className="bg-logo bg-no-repeat bg-center w-44 h-8 md:bg-logo2 md:w-96 md:h-14"></div>
             <div className="bg-gradient-to-r from-azul3 to-azul2 rounded-3xl w-9 h-9 md:w-56 md:flex md:items-center md:justify-around text-white">
                 <img className="w-5 h-5 mr-auto ml-auto mt-2 md:-mt-0 md:ml-9" src="/img/whatsapp.png" alt="" />
