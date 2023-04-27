1c30394dd5886b4756f64ed4894ee965bb5ec02c
Guilherme Vargas <guisouzavargas@outlook.com>
2023-04-27 15:51:35 -0300

    added whatsapp link

--- src/components/presentation/index.tsx
+++ src/components/presentation/index.tsx
@@ -4,18 +4,18 @@ export default function Presentation() {
             <div className="font-bold flex flex-col items-center md:flex-row md:justify-around md:w-full">
                 <div className="flex flex-col md:ml-32 text-center md:text-left">
                     <p className="w-64 md:text-3xl md:w-112">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente esse aliquid tempora enim quas mollitia magnam.</p>
-                    <div className="hidden mt-12 items-center justify-center bg-gradient-to-r from-azul3 to-azul2 rounded-3xl w-80 h-12 text-white md:flex">
+                    <a className="hidden mt-12 items-center justify-center bg-gradient-to-r from-azul3 to-azul2 rounded-3xl w-80 h-12 text-white md:flex" href="https://whatsa.me/5541992295513/?t=Ol%C3%A1,%20Guilherme.%20" target="_blank">
                         <img className="w-5 h-5" src="/img/whatsapp.png" alt="" />
                         <p className="ml-2">Vamos conversar</p>
-                    </div>
+                    </a>
                 </div>
                 <img className="w-52 h-72 mt-11 md:hidden" src="/img/foguete.png" alt="" />
                 <img className="hidden md:inline-block w-112 h-120 md:mr-24" src="/img/foguete@2x.png" alt="" />
             </div>
-            <div className="flex items-center justify-center bg-gradient-to-r from-azul3 to-azul2 rounded-3xl w-80 h-12 text-white md:hidden">
+            <a className="flex items-center justify-center bg-gradient-to-r from-azul3 to-azul2 rounded-3xl w-80 h-12 text-white md:hidden" href="https://whatsa.me/5541992295513/?t=Ol%C3%A1,%20Guilherme.%20" target="_blank">
                 <img className="w-5 h-5" src="/img/whatsapp.png" alt="" />
                 <p className="ml-2">Vamos conversar</p>
-            </div>
+            </a>
             <div className="mt-16">
                 <img src="/img/seta-baixo.png" alt="" />
             </div>
