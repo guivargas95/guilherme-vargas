e9129d0cbb2ce0c7a7148569b538686aef1f591f
Guilherme Vargas <guisouzavargas@outlook.com>
2023-04-30 21:24:16 -0300

    test

--- src/components/presentation/index.tsx
+++ src/components/presentation/index.tsx
@@ -1,24 +1,7 @@
 export default function Presentation() {
     return (
         <section className="flex flex-col items-center text-white mt-20">
-            <div className="font-bold flex flex-col items-center md:flex-row md:justify-around md:w-full">
-                <div className="flex flex-col md:ml-32 text-center md:text-left backInLeft">
-                    <p className="w-64 md:text-3xl md:w-112">Hi, I'm Guilherme, front-end developer and support analyst. I have about five year in IT area and I'm here to help you! </p>
-                    <a className="hidden mt-12 items-center justify-center bg-gradient-to-r from-azul3 to-azul2 rounded-3xl w-80 h-12 text-white md:flex" href="https://whatsa.me/5541992295513/?t=Ol%C3%A1,%20Guilherme.%20" target="_blank">
-                        <img className="w-5 h-5" src="/img/whatsapp.png" alt="" />
-                        <p className="ml-2">Contact me</p>
-                    </a>
-                </div>
-                <img className="w-52 h-72 mt-11 md:hidden backInRight" src="/img/foguete.png" alt="" />
-                <img className="hidden md:inline-block w-112 h-120 md:mr-24 backInRight" src="/img/foguete@2x.png" alt="" />
-            </div>
-            <a className="flex items-center justify-center bg-gradient-to-r from-azul3 to-azul2 rounded-3xl w-80 h-12 text-white md:hidden" href="https://whatsa.me/5541992295513/?t=Ol%C3%A1,%20Guilherme.%20" target="_blank">
-                <img className="w-5 h-5" src="/img/whatsapp.png" alt="" />
-                <p className="ml-2">Contact me</p>
-            </a>
-            <div className="mt-16">
-                <img src="/img/seta-baixo.png" alt="" />
-            </div>
+            
         </section>
     )
 }
